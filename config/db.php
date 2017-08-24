caedcd4cd493dc705de2be9929ae3f36f465a048
Sammy Barasa <sammy.barasa@pristone.net>
2017-08-24 13:37:56 +0300

    database update

--- config/db.php
+++ config/db.php
@@ -2,14 +2,16 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'dsn' => 'sqlite:@app/database/expense_tracker.db',
-    //'username' => 'root',
-    //'password' => '',
-    //'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=tsobucok_expense', // MySQL, MariaDB
+    //'dsn' => 'sqlite:@app/database/expense_tracker.db',
+    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
+    //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
+    //'dsn' => 'sqlsrv:Server=localhost;Database=mydatabase', // MS SQL Server, sqlsrv driver
+    //'dsn' => 'dblib:host=localhost;dbname=mydatabase', // MS SQL Server, dblib driver
+    //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
+    //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
+    //'tablePrefix'=>'tb_',
+    'username' => 'tsobucok_spa',
+    'password' => 'Cyberhopper123',
+    'charset' => 'utf8',
 ];
