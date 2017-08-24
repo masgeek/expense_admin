f1ee0b2c0dfff117eb93dd0d4938e580932568ab
Sammy Barasa <barsamms@gmail.com>
2017-08-24 21:31:38 +0300

    update

--- api/modules/v1/controllers/ExpenseController.php
+++ api/modules/v1/controllers/ExpenseController.php
@@ -9,6 +9,8 @@
 namespace app\api\modules\v1\controllers;
 
 
+use app\api\modules\v1\models\EXPENSE_MODEL;
+use app\api\modules\v1\models\INCOME_MODEL;
 use app\api\modules\v1\models\LOGIN_MODEL;
 use Yii;
 use yii\data\ActiveDataProvider;
@@ -21,69 +23,79 @@ use yii\rest\Controller;
 use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
-use app\models_extended\EXPENSE_MODEL;
 
 class ExpenseController extends ActiveController
 {
-    public $modelClass = 'app\api\modules\v1\models\EXPENSE_MODEL';
+	public $modelClass = 'app\api\modules\v1\models\EXPENSE_MODEL';
 
-    public function actionAdd()
-    {
-        /* @var $request LOGIN_MODEL */
-        $message = [];
+	public function actionAdd()
+	{
+		/* @var $request LOGIN_MODEL */
+		$message = [];
 
-        if (!Yii::$app->request->isPost) {
-            throw new BadRequestHttpException('Please use POST');
-        }
-        $request = (object)Yii::$app->request->post();
+		if (!Yii::$app->request->isPost) {
+			throw new BadRequestHttpException('Please use POST');
+		}
+		$request = (object)Yii::$app->request->post();
 
 
-        $expense = new EXPENSE_MODEL();
-        //$user->setScenario(USER_MODEL::SCENARIO_CREATE);
-        //assign the post data values
-        $expense->type = isset($request->type) ? $request->type : null;
-        $expense->amount = isset($request->amount) ? $request->amount : null;
-        $expense->place = isset($request->place) ? $request->place : null;
-        $expense->note = isset($request->note) ? $request->note : null;
-        $expense->date = isset($request->date) ? $request->date : null;
-        $expense->cheque = isset($request->cheque) ? $request->cheque : null;
+		$expense = new EXPENSE_MODEL();
+		//$user->setScenario(USER_MODEL::SCENARIO_CREATE);
+		//assign the post data values
+		$expense->type = isset($request->type) ? $request->type : null;
+		$expense->amount = isset($request->amount) ? $request->amount : null;
+		$expense->place = isset($request->place) ? $request->place : null;
+		$expense->note = isset($request->note) ? $request->note : null;
+		$expense->date = isset($request->date) ? $request->date : null;
+		$expense->cheque = isset($request->cheque) ? $request->cheque : null;
 
-        if ($expense->validate()) {
-            if ($expense->save()) {
-                $message = [$expense];
-            }
-        } else {
-            $errors = $expense->getErrors();
-            foreach ($errors as $key => $error) {
-                $message[] = [
-                    'field' => $key,
-                    'message' => $error[0]
-                ];
-            }
-        }
-        return $message;
-    }
+		if ($expense->validate()) {
+			if ($expense->save()) {
+				$message = [$expense];
+			}
+		} else {
+			$errors = $expense->getErrors();
+			foreach ($errors as $key => $error) {
+				$message[] = [
+					'field' => $key,
+					'message' => $error[0]
+				];
+			}
+		}
+		return $message;
+	}
 
-    public function actionCategory()
-    {
-        $query = EXPENSE_MODEL::find();
+	public function actionCategory()
+	{
+		$query = EXPENSE_MODEL::find();
 
-        // The problem is in the below sum
-        // $query->joinWith('inventory');
-        $query->select(['type', 'amount']);
-        $query->groupBy('type');
-        $query->sum('amount');
-        $query->all();
+		// The problem is in the below sum
+		// $query->joinWith('inventory');
+		$query->select(['type', 'amount']);
+		$query->groupBy('type');
+		$query->sum('amount');
+		$query->all();
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 
-    public function actionAll()
-    {
+	public function actionAll()
+	{
+		$expenses = EXPENSE_MODEL::find()->sum('amount');
+		$income = INCOME_MODEL::find()->sum('amount');
 
-    }
+		$difference = ((float)$income) - ((float)$expenses);
+
+		$msg = [
+			'incomme'=>$income,
+			'expenses'=>$expenses,
+			'difference'=>(string)$difference,
+		];
+		return $msg;
+	}
+
 }
