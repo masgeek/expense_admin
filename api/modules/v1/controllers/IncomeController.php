3b07a6a308e90c71358bec038f892fb10f944323
Sammy Barasa <barsamms@gmail.com>
2017-08-24 22:15:50 +0300

    updates

--- api/modules/v1/controllers/IncomeController.php
+++ api/modules/v1/controllers/IncomeController.php
@@ -26,59 +26,77 @@ use app\models_extended\EXPENSE_MODEL;
 
 class IncomeController extends ActiveController
 {
-    public $modelClass = 'app\api\modules\v1\models\INCOME_MODEL';
+	public $modelClass = 'app\api\modules\v1\models\INCOME_MODEL';
 
-    public function actionAdd()
-    {
-        $message = [];
+	public function actionAdd()
+	{
+		$message = [];
 
-        if (!Yii::$app->request->isPost) {
-            throw new BadRequestHttpException('Please use POST');
-        }
-        $request = (object)Yii::$app->request->post();
+		if (!Yii::$app->request->isPost) {
+			throw new BadRequestHttpException('Please use POST');
+		}
+		$request = (object)Yii::$app->request->post();
 
 
-        $expense = new INCOME_MODEL();
-        //$user->setScenario(USER_MODEL::SCENARIO_CREATE);
-        //assign the post data values
-        $expense->type = isset($request->type) ? $request->type : null;
-        $expense->amount = isset($request->amount) ? $request->amount : null;
-        $expense->place = isset($request->place) ? $request->place : null;
-        $expense->note = isset($request->note) ? $request->note : null;
-        $expense->date = isset($request->date) ? $request->date : null;
-        $expense->cheque = isset($request->cheque) ? $request->cheque : null;
+		$expense = new INCOME_MODEL();
+		//$user->setScenario(USER_MODEL::SCENARIO_CREATE);
+		//assign the post data values
+		$expense->type = isset($request->type) ? $request->type : null;
+		$expense->amount = isset($request->amount) ? $request->amount : null;
+		$expense->place = isset($request->place) ? $request->place : null;
+		$expense->note = isset($request->note) ? $request->note : null;
+		$expense->date = isset($request->date) ? $request->date : null;
+		$expense->cheque = isset($request->cheque) ? $request->cheque : null;
 
-        if ($expense->validate()) {
-            if ($expense->save()) {
-                $message = [$expense];
-            }
-        } else {
-            $errors = $expense->getErrors();
-            foreach ($errors as $key => $error) {
-                $message[] = [
-                    'field' => $key,
-                    'message' => $error[0]
-                ];
-            }
-        }
-        return $message;
-    }
+		if ($expense->validate()) {
+			if ($expense->save()) {
+				$message = [$expense];
+			}
+		} else {
+			$errors = $expense->getErrors();
+			foreach ($errors as $key => $error) {
+				$message[] = [
+					'field' => $key,
+					'message' => $error[0]
+				];
+			}
+		}
+		return $message;
+	}
 
-    public function actionCategory()
-    {
-        $query = INCOME_MODEL::find();
+	public function actionCategory()
+	{
+		$query = INCOME_MODEL::find();
 
-        // The problem is in the below sum
-        // $query->joinWith('inventory');
-        $query->select(['type', 'amount']);
-        $query->groupBy('type');
-        $query->sum('amount');
-        $query->all();
+		// The problem is in the below sum
+		// $query->joinWith('inventory');
+		$query->select(['type', 'amount']);
+		$query->groupBy('type');
+		$query->sum('amount');
+		$query->all();
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
+
+	public function actionGraph()
+	{
+		$points = [];
+		$expenses = \app\api\modules\v1\models\INCOME_MODEL::find()
+			->orderBy(['date' => SORT_ASC])
+			->all();
+		$i = 0;
+		foreach ($expenses as $key => $value) {
+			$points[] = [
+				'point' => $i,
+				'value' => (int)$value->amount
+			];
+
+			$i++;
+		}
+		return $points;
+	}
 }
